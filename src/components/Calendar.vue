344bc565c456303f6e8608085a0f5586fe72abd2
Denis Butsky <nthbutsky@gmail.com>
2021-11-29 17:29:08 +0200

    fix calendar layout

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,203 +1,205 @@
 <template>
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar flat>
-          <v-btn small color="secondary" dark @click.stop="dialog = true">
-            New Event
-          </v-btn>
-          <v-btn small outlined class="mr-4" @click="setToday"> Today </v-btn>
-          <v-btn fab text small @click="prev">
-            <v-icon small>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn fab text small @click="next">
-            <v-icon small>mdi-chevron-right</v-icon>
-          </v-btn>
-          <v-toolbar-title>{{ title }}</v-toolbar-title>
-          <div class="flex-grow-1"></div>
-          <v-menu bottom right>
-            <template v-slot:activator="{ on }">
-              <v-btn small outlined v-on="on">
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>mdi-menu-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
+  <v-app>
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet height="64">
+          <v-toolbar flat>
+            <v-btn small color="secondary" dark @click.stop="dialog = true">
+              New Event
+            </v-btn>
+            <v-btn small outlined class="mr-4" @click="setToday"> Today </v-btn>
+            <v-btn fab text small @click="prev">
+              <v-icon small>mdi-chevron-left</v-icon>
+            </v-btn>
+            <v-btn fab text small @click="next">
+              <v-icon small>mdi-chevron-right</v-icon>
+            </v-btn>
+            <v-toolbar-title>{{ title }}</v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <v-menu bottom right>
+              <template v-slot:activator="{ on }">
+                <v-btn small outlined v-on="on">
+                  <span>{{ typeToLabel[type] }}</span>
+                  <v-icon right>mdi-menu-down</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="type = 'day'">
+                  <v-list-item-title>Day</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'week'">
+                  <v-list-item-title>Week</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'month'">
+                  <v-list-item-title>Month</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = '4day'">
+                  <v-list-item-title>4 days</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+        </v-sheet>
 
-      <v-dialog v-model="dialog" max-width="500">
-        <v-card>
-          <v-container>
-            <v-form @submit.prevent="addEvent">
-              <v-text-field
-                v-model="name"
-                type="text"
-                label="event name (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="details"
-                type="text"
-                label="detail"
-              ></v-text-field>
-              <v-text-field
-                v-model="start"
-                type="date"
-                label="start (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="end"
-                type="date"
-                label="end (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="color"
-                type="color"
-                label="color (click to open color menu)"
-              ></v-text-field>
-              <v-btn
-                block
-                type="submit"
-                color="secondary"
-                class="mr-4"
-                @click.stop="dialog = false"
-              >
-                create event
-              </v-btn>
-            </v-form>
-          </v-container>
-        </v-card>
-      </v-dialog>
-
-      <v-dialog v-model="dialogDate" max-width="500">
-        <v-card>
-          <v-container>
-            <v-form @submit.prevent="addEvent">
-              <v-text-field
-                v-model="name"
-                type="text"
-                label="event name (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="details"
-                type="text"
-                label="detail"
-              ></v-text-field>
-              <v-text-field
-                v-model="start"
-                type="date"
-                label="start (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="end"
-                type="date"
-                label="end (required)"
-              ></v-text-field>
-              <v-text-field
-                v-model="color"
-                type="color"
-                label="color (click to open color menu)"
-              ></v-text-field>
-              <v-btn
-                block
-                type="submit"
-                color="secondary"
-                class="mr-4"
-                @click.stop="dialog = false"
-              >
-                create event
-              </v-btn>
-            </v-form>
-          </v-container>
-        </v-card>
-      </v-dialog>
-
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :event-margin-bottom="3"
-          :now="today"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="setDialogDate"
-          @change="updateRange"
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          full-width
-          offset-x
-        >
-          <v-card color="grey lighten-4" :width="350" flat>
-            <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <div class="flex-grow-1"></div>
-            </v-toolbar>
-
-            <v-card-text>
-              <form v-if="currentlyEditing !== selectedEvent.id">
-                {{ selectedEvent.details }}
-              </form>
-              <form v-else>
-                <textarea-autosize
-                  v-model="selectedEvent.details"
+        <v-dialog v-model="dialog" max-width="500">
+          <v-card>
+            <v-container>
+              <v-form @submit.prevent="addEvent">
+                <v-text-field
+                  v-model="name"
                   type="text"
-                  style="width: 100%"
-                  :min-height="100"
-                  placeholder="add note"
+                  label="event name (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="details"
+                  type="text"
+                  label="detail"
+                ></v-text-field>
+                <v-text-field
+                  v-model="start"
+                  type="date"
+                  label="start (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="end"
+                  type="date"
+                  label="end (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="color"
+                  type="color"
+                  label="color (click to open color menu)"
+                ></v-text-field>
+                <v-btn
+                  block
+                  type="submit"
+                  color="secondary"
+                  class="mr-4"
+                  @click.stop="dialog = false"
                 >
-                </textarea-autosize>
-              </form>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-btn text color="secondary" @click="selectedOpen = false">
-                close
-              </v-btn>
-              <v-btn
-                v-if="currentlyEditing !== selectedEvent.id"
-                text
-                @click.prevent="editEvent(selectedEvent)"
-              >
-                edit
-              </v-btn>
-              <v-btn
-                text
-                v-else
-                type="submit"
-                @click.prevent="updateEvent(selectedEvent)"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
+                  create event
+                </v-btn>
+              </v-form>
+            </v-container>
           </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
+        </v-dialog>
+
+        <v-dialog v-model="dialogDate" max-width="500">
+          <v-card>
+            <v-container>
+              <v-form @submit.prevent="addEvent">
+                <v-text-field
+                  v-model="name"
+                  type="text"
+                  label="event name (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="details"
+                  type="text"
+                  label="detail"
+                ></v-text-field>
+                <v-text-field
+                  v-model="start"
+                  type="date"
+                  label="start (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="end"
+                  type="date"
+                  label="end (required)"
+                ></v-text-field>
+                <v-text-field
+                  v-model="color"
+                  type="color"
+                  label="color (click to open color menu)"
+                ></v-text-field>
+                <v-btn
+                  block
+                  type="submit"
+                  color="secondary"
+                  class="mr-4"
+                  @click.stop="dialog = false"
+                >
+                  create event
+                </v-btn>
+              </v-form>
+            </v-container>
+          </v-card>
+        </v-dialog>
+
+        <v-sheet height="600">
+          <v-calendar
+            ref="calendar"
+            v-model="focus"
+            color="primary"
+            :events="events"
+            :event-color="getEventColor"
+            :event-margin-bottom="3"
+            :now="today"
+            :type="type"
+            @click:event="showEvent"
+            @click:more="viewDay"
+            @click:date="setDialogDate"
+            @change="updateRange"
+          ></v-calendar>
+          <v-menu
+            v-model="selectedOpen"
+            :close-on-content-click="false"
+            :activator="selectedElement"
+            full-width
+            offset-x
+          >
+            <v-card color="grey lighten-4" :width="350" flat>
+              <v-toolbar :color="selectedEvent.color" dark>
+                <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <div class="flex-grow-1"></div>
+              </v-toolbar>
+
+              <v-card-text>
+                <form v-if="currentlyEditing !== selectedEvent.id">
+                  {{ selectedEvent.details }}
+                </form>
+                <form v-else>
+                  <textarea-autosize
+                    v-model="selectedEvent.details"
+                    type="text"
+                    style="width: 100%"
+                    :min-height="100"
+                    placeholder="add note"
+                  >
+                  </textarea-autosize>
+                </form>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn text color="secondary" @click="selectedOpen = false">
+                  close
+                </v-btn>
+                <v-btn
+                  v-if="currentlyEditing !== selectedEvent.id"
+                  text
+                  @click.prevent="editEvent(selectedEvent)"
+                >
+                  edit
+                </v-btn>
+                <v-btn
+                  text
+                  v-else
+                  type="submit"
+                  @click.prevent="updateEvent(selectedEvent)"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script>

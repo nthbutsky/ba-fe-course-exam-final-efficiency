d56139bf5268987b1d79244264b6ce000d65043b
Denis Butsky <nthbutsky@gmail.com>
2021-12-13 19:04:36 +0200

    do vuex modules

---
+++ src/store/modules/news.js
@@ -0,0 +1,29 @@
+import { newsApiKey } from "../../api/news.config";
+
+export default {
+  state: { news: null },
+
+  getters: {
+    news: (state) => state.news,
+  },
+
+  mutations: {
+    setNews: (state, payload) => (state.news = payload),
+  },
+
+  actions: {
+    async getNews({ commit }) {
+      try {
+        const apiKey = newsApiKey;
+        const newsResponse = await fetch(
+          "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=" +
+            apiKey
+        );
+        const newsData = await newsResponse.json();
+        commit("setNews", newsData);
+      } catch (error) {
+        commit("setError", error.message);
+      }
+    },
+  },
+};

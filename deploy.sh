324d5e4bb72a69717a34ca5f001c5e09e6eac4aa
Denis Butsky <butskyi@convidera.com>
2022-12-07 14:53:28 -0800

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:nthbutsky/efficiency-app.git chief:gh-pages
+git push -f git@github.com:nthbutsky/efficiency-app.git master:gh-pages
 cd -
 rm -rf dist
